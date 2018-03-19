5ab4580047740f13b5c1bad7a71c76de91c98038
dylanhross <DylanRoss@Dylans-MacBook-Pro.local>
2018-03-19 11:02:05 -0700

    newest version here
    totally rewrote the VM from the bottom up with a vaguely object
    oriented style

---
+++ test.c
@@ -0,0 +1,102 @@
+/*
+    C16_VM_v3
+    Dylan H. Ross
+    2017/11/24
+    
+    The third (and I pray final) iteration of my toy 16-bit virtual machine.
+    
+    test.c
+*/
+
+
+#include "cpu.h"
+#include <stdio.h>
+
+
+void print_memrange(sysmem_t*, uint16_t, uint16_t);
+void print_cpuregs(core_t*);
+
+
+int main() {
+    
+    /* SETUP */
+    sysmem_t *smem = sysmem_init();
+    core_t *core0 = core_init(0, smem);
+    
+    
+    core0->seti(core0, IR0, 0xBEEF);
+    core0->seti(core0, IR1, 0x1234);
+    core0->seti(core0, IR2, 0xF00D);
+    core0->seti(core0, IR3, 0x03E8);
+    core0->setf(core0, FR0, 0.1);
+    core0->setf(core0, FR1, 0.01);
+    core0->setf(core0, FR2, 0.001);
+    core0->setf(core0, FR3, 0.0001);
+    core0->leai(core0, RSP, IR3, 0, IRV);
+    
+    
+    printf("--------------------------------------------------------\n");
+    print_cpuregs(core0);
+    print_memrange(smem, 0xF05F, 0xF0AF);
+    printf("\n");
+    
+    core0->call(core0, 0x1111);
+    core0->seti(core0, IR0, 0x0001);
+    core0->seti(core0, IR1, 0x0002);
+    core0->seti(core0, IR2, 0x0003);
+    core0->seti(core0, IR3, 0x0004);
+    core0->addi(core0, IR0, IR1);
+    core0->addi(core0, IR2, IR3);
+    core0->subi(core0, IR1, IR3);
+    core0->movi(core0, IR3, IRV);
+    
+
+    
+    printf("--------------------------------------------------------\n");
+    print_cpuregs(core0);
+    print_memrange(smem, 0xF05F, 0xF0AF);
+    printf("\n");
+    
+    core0->retn(core0);
+    
+    printf("--------------------------------------------------------\n");
+    print_cpuregs(core0);
+    print_memrange(smem, 0xF05F, 0xF0AF);
+    printf("\n");
+    
+    
+    /* FINISH */
+    core_delete(core0);
+    sysmem_delete(smem);
+
+    return 0;
+}
+
+
+void print_memrange(sysmem_t *smem, uint16_t start, uint16_t end) {
+    uint16_t r = (end - start) / 16;
+    r = (end - start) % 16 ? r + 1 : r;
+    for (uint16_t i = 0; i < r; i++) {
+        printf("[0x%04X] ", i * 16 + start);
+        uint16_t j = 0;
+        uint16_t jaddr = i * 16 + start;
+        while (j < 16 && jaddr < end) {
+            printf("%02X ", *(smem->mem + jaddr));
+            j++;
+            jaddr++;
+        }
+        printf("\n");
+    }
+    
+}
+
+
+void print_cpuregs(core_t *core) {
+    printf("! STC: %d !\n", core->stc);
+    printf("RPC: 0x%04X\n", core->rpc);
+    printf("RSP: 0x%04X\n", core->rsp);
+    printf("RBP: 0x%04X\n", core->rbp);
+    printf("     IR0     IR1     IR2     IR3     IRV\n");
+    printf("   %5d   %5d   %5d   %5d   %5d\n", core->ir0, core->ir1, core->ir2, core->ir3, core->irv);
+    printf("  0x%04X  0x%04X  0x%04X  0x%04X  0x%04X\n", core->ir0, core->ir1, core->ir2, core->ir3, core->irv);
+}
